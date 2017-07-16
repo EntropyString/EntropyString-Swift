4dcb78c87d209bd50941e0d4d800ccba42a0ea2a
dingo sky <paul@dingosky.com>
2017-07-16 13:43:29 -0700

    Travis macOS & tvOS

--- ci/travis.sh
+++ ci/travis.sh
@@ -4,14 +4,29 @@ set -e -o pipefail
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     xcodebuild -version
+
     xcodebuild \
-        -project $FRAMEWORK_NAME.xcodeproj \
-        -scheme "$FRAMEWORK_NAME-iOS" \
+        -project ${FRAMEWORK_NAME}.xcodeproj \
+        -scheme "${$FRAMEWORK_NAME}-iOS" \
         -sdk iphonesimulator \
         -destination "platform=iOS Simulator,name=iPhone 7,OS=10.3.1" \
         ONLY_ACTIVE_ARCH=NO \
         test | xcpretty
 
+    xcodebuild \
+        -project ${FRAMEWORK_NAME}.xcodeproj \
+        -scheme "${$FRAMEWORK_NAME}-tvOS" \
+        -sdk appletvsimulator \
+        -destination "platform=tvOS Simulator,name=Apple TV 1080p" \
+        ONLY_ACTIVE_ARCH=NO \
+        test | xcpretty
+
+    xcodebuild \
+        -project ${FRAMEWORK_NAME}.xcodeproj \
+        -scheme "${$FRAMEWORK_NAME}-macOS" \
+        ONLY_ACTIVE_ARCH=YES \
+        test | xcpretty
+    
     pod lib lint --quick
 fi
 
