a33c5eaca1bc224679e88aff32533b8fc17059ea
dingo sky <paul@dingosky.com>
2017-07-26 21:19:42 -0700

    TLDR 2 tweak

--- EntropyString.playground/Pages/TLDR2.xcplaygroundpage/Contents.swift
+++ EntropyString.playground/Pages/TLDR2.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
 //:
 //:  - You don't need random strings of length L.
 //:    - String length is a by-product, not a goal.
-//:  - You need unique strings.
+//:  - You don't need truly unique strings.
 //:    - Uniqueness is too onerous. You'll do fine with probabilistically unique strings.
 //:  - Probabilistic uniqueness involves measured risk.
 //:    - Risk is measured as *"1 in __n__ chance of generating a repeat"*
